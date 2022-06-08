67931767280401024fdef8d470abe3465901bb62
Henrique Tschannerl <h.tschannerl@gmail.com>
2022-06-07 21:15:54 -0300

    Initial Commit

---
+++ install.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+sudo apt install zsh git
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+git clone https://github.com/popstas/zsh-command-time.git ~/.oh-my-zsh/custom/plugins/command-time
