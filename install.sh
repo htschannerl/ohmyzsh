134d5e343fce7058a54936350f410d10e69a5f90
Henrique Tschannerl <h.tschannerl@gmail.com>
2022-06-07 21:19:58 -0300

    Adicionado a cópia do arquivo de configuração

--- install.sh
+++ install.sh
@@ -2,3 +2,4 @@
 sudo apt install zsh git
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 git clone https://github.com/popstas/zsh-command-time.git ~/.oh-my-zsh/custom/plugins/command-time
+wget -O ~/.zshrc https://raw.githubusercontent.com/htschannerl/ohmyzsh/main/.zshrc
