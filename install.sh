a385e97d6e1435ea21a24363ab8d0a4016887479
Henrique Tschannerl <h.tschannerl@gmail.com>
2023-01-05 09:45:09 -0300

    Created action and highlights plugins

--- install.sh
+++ install.sh
@@ -2,4 +2,5 @@
 sudo apt install zsh git
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 git clone https://github.com/popstas/zsh-command-time.git ~/.oh-my-zsh/custom/plugins/command-time
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 wget -O ~/.zshrc https://raw.githubusercontent.com/htschannerl/ohmyzsh/main/.zshrc
